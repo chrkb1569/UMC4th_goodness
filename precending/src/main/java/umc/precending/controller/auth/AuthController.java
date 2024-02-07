9be5ccf3b5e2bb6d8e0bc185bcb409fe819d3aa8
minsub <chrkb1569@naver.com>
2024-02-07 19:29:22 +0900

    refactor : Swagger 버전 변경에 의한 Annotation 수정

--- precending/src/main/java/umc/precending/controller/auth/AuthController.java
+++ precending/src/main/java/umc/precending/controller/auth/AuthController.java
@@ -1,60 +1,57 @@
 package umc.precending.controller.auth;
 
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import umc.precending.dto.auth.*;
+import umc.precending.dto.auth.MemberClubSignUpDto;
+import umc.precending.dto.auth.MemberCorporateSignUpDto;
+import umc.precending.dto.auth.MemberPersonSignUpDto;
+import umc.precending.dto.auth.MemberSignInDto;
 import umc.precending.dto.token.TokenRequestDto;
 import umc.precending.response.Response;
 import umc.precending.service.auth.AuthService;
 
-import javax.validation.Valid;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Tag(name = "사용자 인증 API", description = "회원가입, 로그인과 같은 인증과 관련된 로직을 수행하기 위한 Controller입니다.")
 public class AuthController {
-
     private final AuthService authService;
 
     @PostMapping("/auth/sign-up/person")
     @ResponseStatus(HttpStatus.CREATED)
-    @ApiOperation(value = "회원가입 - 개인", notes = "개인 회원으로 가입하기 위한 로직")
-    @ApiImplicitParam(name = "signUpDto", value = "회원가입을 위한 데이터가 담겨진 DTO")
+    @Operation(method = "POST", summary = "개인 사용자 회원 가입", description = "개인 사용자가 회원 가입하기 위한 API")
     public void signUp(@RequestBody @Valid MemberPersonSignUpDto signUpDto) {
         authService.signUp(signUpDto);
     }
 
     @PostMapping("/auth/sign-up/corporate")
     @ResponseStatus(HttpStatus.CREATED)
-    @ApiOperation(value = "회원가입 - 기업", notes = "단체 회원 - 기업으로 가입하기 위한 로직")
-    @ApiImplicitParam(name = "signUpDto", value = "회원가입을 위한 데이터가 담겨진 DTO")
+    @Operation(method = "POST", summary = "기업 사용자 회원 가입", description = "기업이 회원 가입하기 위한 API")
     public void signUp(@RequestBody @Valid MemberCorporateSignUpDto signUpDto) {
         authService.signUp(signUpDto);
     }
 
     @PostMapping("/auth/sign-up/club")
     @ResponseStatus(HttpStatus.CREATED)
-    @ApiOperation(value = "회원가입 - 동아리", notes = "단체 회원 - 동아리로 가입하기 위한 로직")
-    @ApiImplicitParam(name = "signUpDto", value = "회원가입을 위한 데이터가 담겨진 DTO")
+    @Operation(method = "POST", summary = "동아리 사용자 회원 가입", description = "동아리가 회원 가입하기 위한 API")
     public void signUp(@RequestBody @Valid MemberClubSignUpDto signUpDto) {
         authService.signUp(signUpDto);
     }
 
     @PostMapping("/auth/sign-in")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "로그인", notes = "로그인하기 위한 로직")
-    @ApiImplicitParam(name = "signInDto", value = "로그인을 위한 데이터가 담겨진 DTO")
+    @Operation(method = "POST", summary = "로그인", description = "로그인을 수행하기 위한 API")
     public Response signIn(@RequestBody @Valid MemberSignInDto signInDto) {
         return Response.success(authService.signIn(signInDto));
     }
 
     @PostMapping("/auth/reissue")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "토큰 재발급", notes = "만료 기간이 완료된 토큰을 재발급하는 로직")
-    @ApiImplicitParam(name = "requestDto", value = "토큰 재발급에 필요한 정보들이 담긴 DTO")
+    @Operation(method = "POST", summary = "토큰 재발급", description = "유효기간이 만료된 JWT 토큰을 재발급받기 위한 API")
     public Response reIssue(@RequestBody @Valid TokenRequestDto requestDto) {
         return Response.success(authService.reIssue(requestDto));
     }
