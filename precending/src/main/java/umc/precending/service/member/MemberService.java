06e0f8e4fa08e1fae644a919e39eafb63d89b4a2
minsub <chrkb1569@naver.com>
2024-02-07 19:36:51 +0900

    refactor : Member Domain 관련 Service 수정

--- precending/src/main/java/umc/precending/service/member/MemberService.java
+++ precending/src/main/java/umc/precending/service/member/MemberService.java
@@ -7,48 +7,104 @@ import org.springframework.web.multipart.MultipartFile;
 import umc.precending.domain.image.Image;
 import umc.precending.domain.image.MemberImage;
 import umc.precending.domain.member.Member;
-import umc.precending.dto.person.MemberUpdateRequestDto;
+import umc.precending.dto.member.MemberInfoDto;
+import umc.precending.dto.member.MemberResponseDto;
+import umc.precending.dto.member.MemberUpdateDto;
+import umc.precending.exception.member.MemberNotFoundException;
 import umc.precending.repository.member.MemberRepository;
 import umc.precending.service.image.ImageService;
 
 import java.util.List;
 
+import static umc.precending.domain.member.Authority.*;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
-    private final ImageService imageService;
+    private final MemberFindService memberFindService;
     private final MemberRepository memberRepository;
+    private final ImageService imageService;
 
-    // 회원의 프로필을 설정하는 로직
+    @Transactional(readOnly = true)
+    public MemberInfoDto getMemberInfo(Member member) {
+        return null;
+    }
+
+    // 회원의 프로필 이미지를 변경하는 로직
     @Transactional
-    public void saveImage(MultipartFile file, Member member) {
+    public void editMemberImage(MultipartFile file, Member member) {
         Image image = getMemberImage(file);
         saveMemberImage(file, image);
 
-        member.saveImage(List.of((MemberImage)image));
+        member.editImage(List.of((MemberImage) image));
     }
 
-    public Image getMemberImage(MultipartFile file) {
+    // 조건에 일치하는 기업 회원들을 검색하는 로직
+    @Transactional(readOnly = true)
+    public List<MemberResponseDto> findCorporateMember(Member member, String name) {
+        List<MemberResponseDto> responseData = memberFindService.findMemberByNameAndAuthority(ROLE_CORPORATE, name);
+        if(responseData.isEmpty()) throw new MemberNotFoundException();
+
+        return responseData;
+    }
+
+    // 조건에 일치하는 동아리 회원들을 검색하는 로직
+    @Transactional(readOnly = true)
+    public List<MemberResponseDto> findClubMember(Member member, String name) {
+        List<MemberResponseDto> responseData = memberFindService.findMemberByNameAndAuthority(ROLE_CLUB, name);
+        if(responseData.isEmpty()) throw new MemberNotFoundException();
+
+        return responseData;
+    }
+
+    // 회원의 이름이나 소개글을 변경하는 로직
+    @Transactional
+    public void updateMember(Member member, MemberUpdateDto request) {
+        updateMemberInfo(member, request);
+    }
+
+    // 회원을 탈퇴하는 로직
+    @Transactional
+    public void deleteMember(Member member) {
+        memberRepository.delete(member);
+    }
+
+    private Image getMemberImage(MultipartFile file) {
         return new MemberImage(file);
     }
 
-    public void saveMemberImage(MultipartFile file, Image image) {
+    private void saveMemberImage(MultipartFile file, Image image) {
         String storedName = image.getStoredName();
 
         String accessUrl = imageService.saveImage(file, storedName);
         image.setAccessUrl(accessUrl);
     }
 
-    // 회원정보 수정 로직
-    @Transactional
-    public void updateMember(Member member, MemberUpdateRequestDto request) {
-        member.update(request);
-        memberRepository.save(member);
+    private void updateMemberInfo(Member member, MemberUpdateDto updateDto) {
+        String name = updateDto.getName();
+        String introduction = updateDto.getIntroduction();
+
+        if(checkNameValidation(name) && checkIntroductionValidation(introduction)) return;
+
+        if(checkNameValidation(name)) {
+            member.editIntroduction(introduction);
+            return;
+        }
+
+        if(checkIntroductionValidation(introduction)) {
+            member.editName(name);
+            return;
+        }
+
+        member.editName(name);
+        member.editIntroduction(introduction);
     }
 
-    // 회원탈퇴
-    @Transactional
-    public void deleteMember(Member member) {
-        memberRepository.delete(member);
+    private boolean checkNameValidation(String name) {
+        return name.isBlank() || name.isEmpty();
+    }
+
+    private boolean checkIntroductionValidation(String introduction) {
+        return introduction.isEmpty() || introduction.isBlank();
     }
 }
