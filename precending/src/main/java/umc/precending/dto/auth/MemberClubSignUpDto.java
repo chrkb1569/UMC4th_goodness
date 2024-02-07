e0b46e3e28a443e221e5467001b90e988a7b3d5a
minsub <chrkb1569@naver.com>
2024-02-07 19:31:47 +0900

    refactor : Domain 변경에 의한 DTO 파일 변경

--- precending/src/main/java/umc/precending/dto/auth/MemberClubSignUpDto.java
+++ precending/src/main/java/umc/precending/dto/auth/MemberClubSignUpDto.java
@@ -1,43 +1,34 @@
 package umc.precending.dto.auth;
 
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
-
-import javax.validation.constraints.NotBlank;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class MemberClubSignUpDto {
     @NotBlank(message = "회원가입에 필요한 이름을 입력해주세요.")
-    @ApiModelProperty(value = "회원가입에 필요한 동아리의 이름", name = "test")
+    @Schema(description = "회원가입에 필요한 이름", example = "이민섭")
     private String name;
 
-    @NotBlank(message = "회원가입에 필요한 동아리 설립일을 입력해주세요.")
-    @ApiModelProperty(value = "회원가입에 필요한 동아리의 설립일(연도, 월)", name = "9999/12")
-    private String birth;
-
-    @NotBlank(message = "회원가입에 필요한 동아리 유형을 입력해주세요.")
-    @ApiModelProperty(value = "회원가입에 필요한 동아리의 유형", name = "type")
-    private String type;
-
     @NotBlank(message = "회원가입에 필요한 주소를 입력해주세요.")
-    @ApiModelProperty(value = "회원가입에 필요한 동아리의 주소", name = "address")
+    @Schema(description = "회원가입에 필요한 동아리의 주소", example = "대한민국 서울특별시")
     private String address;
 
     @NotBlank(message = "회원가입에 필요한 학교명을 입력해주세요.")
-    @ApiModelProperty(value = "회원가입에 필요한 학교명", name = "school")
+    @Schema(description = "회원가입에 필요한 동아리가 속한 학교명", example = "서울대학교")
     private String school;
 
     @NotBlank(message = "회원가입에 필요한 비밀번호를 입력해주세요.")
     @Length(min = 8, max = 16, message = "비밀번호는 최소 8자, 최대 16자로 구성되어야합니다.")
-    @ApiModelProperty(value = "회웝가입에 필요한 비밀번호", name = "1234")
+    @Schema(description = "회원가입에 필요한 비밀번호", example = "1234567890", minLength = 8, maxLength = 16)
     private String password;
 
     @NotBlank(message = "회원가입에 필요한 이메일을 입력해주세요.")
-    @ApiModelProperty(value = "회원가입에 필요한 이메일", name = "test@test.com")
+    @Schema(description = "회원가입에 필요한 이메일", example = "test@test.com")
     private String email;
 }
--- precending/src/main/java/umc/precending/dto/auth/MemberCorporateSignUpDto.java
+++ precending/src/main/java/umc/precending/dto/auth/MemberCorporateSignUpDto.java
@@ -1,36 +1,31 @@
 package umc.precending.dto.auth;
 
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
-
-import javax.validation.constraints.NotBlank;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class MemberCorporateSignUpDto {
     @NotBlank(message = "회원가입에 필요한 이름을 입력해주세요.")
-    @ApiModelProperty(value = "회원가입에 필요한 기업의 이름", example = "test")
+    @Schema(description = "회원가입에 필요한 이름", example = "이민섭")
     private String name;
-
-    @NotBlank(message = "회원가입에 필요한 설립일을 입력해주세요.")
-    @ApiModelProperty(value = "회원가입에 필요한 기업의 설립일(연도, 월)", example = "9999/12")
-    private String birth;
 
     @NotBlank(message = "회원가입에 필요한 사업자번호를 입력해주세요.")
     @Length(max = 12)
-    @ApiModelProperty(value = "회원가입에 필요한 사업자 번호", example = "123-45-67890")
+    @Schema(description = "회원가입에 필요한 사업자 번호", example = "123-45-67890", maxLength = 12)
     private String registrationNumber;
 
     @NotBlank(message = "회원가입에 필요한 비밀번호를 입력해주세요.")
     @Length(min = 8, max = 16, message = "비밀번호는 최소 8자, 최대 16자로 구성되어야합니다.")
-    @ApiModelProperty(value = "회원가입에 필요한 비밀번호", example = "1234")
+    @Schema(description = "회원가입에 필요한 비밀번호", example = "1234567890", minLength = 8, maxLength = 16)
     private String password;
 
     @NotBlank(message = "회원가입에 필요한 이메일을 입력해주세요.")
-    @ApiModelProperty(value = "회원가입에 필요한 이메일", example = "test@test.com")
+    @Schema(description = "회원가입에 필요한 이메일", example = "test@test.com")
     private String email;
 }
