f21059fc738b101960b586772dded19206d34c2b
minsub <chrkb1569@naver.com>
2024-02-10 04:19:08 +0900

    refactor : PostController 리팩터링

--- precending/src/main/java/umc/precending/controller/post/PostController.java
+++ precending/src/main/java/umc/precending/controller/post/PostController.java
@@ -1,33 +1,31 @@
 package umc.precending.controller.post;
 
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import umc.precending.domain.member.Member;
 import umc.precending.dto.post.PostEditDto;
 import umc.precending.dto.post.PostNewsCreateDto;
+import umc.precending.dto.post.PostNewsEditDto;
 import umc.precending.dto.post.PostNormalCreateDto;
-import umc.precending.exception.member.MemberNotFoundException;
-import umc.precending.repository.member.MemberRepository;
 import umc.precending.response.Response;
+import umc.precending.service.member.MemberFindService;
 import umc.precending.service.post.PostService;
-
-import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Tag(name = "게시글 관련 API", description = "게시글 작성, 조회, 수정, 삭제와 관련된 로직을 수행하기 위한 Controller입니다.")
 public class PostController {
-
+    private final MemberFindService memberFindService;
     private final PostService postService;
-    private final MemberRepository memberRepository;
 
     @GetMapping("/posts/{year}/{month}")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "게시글 전체 조회", notes = "사용자가 작성한 모든 게시글들을 조회하는 로직")
+    @Operation(method = "GET", summary = "사용자 전체 게시글 조회 - 선행 일자 기준", description = "사용자의 선행 일자를 기준으로 게시글을 조회하는 API")
     public Response findAll(@PathVariable int year, @PathVariable int month) {
         Member findMember = getMember();
         return Response.success(postService.findAll(year, month, findMember));
@@ -35,76 +33,88 @@ public class PostController {
 
     @GetMapping("/posts/{id}")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "게시글 단일 조회", notes = "게시글의 id를 통하여 특정 게시글을 조회하는 로직")
+    @Operation(method = "GET", summary = "단일 게시글 조회", description = "PK값을 통하여 특정 게시글을 조회하는 API")
     public Response findOne(@PathVariable Long id) {
         return Response.success(postService.findOne(id));
     }
 
     @GetMapping("/posts/corporate")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "최신 기업 게시글 조회", notes = "기업이 작성한 최신 뉴스들을 조회하는 로직")
+    @Operation(method = "GET", summary = "기업 작성 게시글 조회", description = "기업 회원이 작성한 게시글들을 조회하는 API")
     public Response findCorporatePost() {
         return Response.success(postService.getCorporatePost());
     }
 
-    @PostMapping("/posts/valid/news")
-    @ResponseStatus(HttpStatus.CREATED)
-    @ApiOperation(value = "인증 가능 게시글 생성 - 기사", notes = "인증 가능한 기사 게시글을 생성하는 로직")
-    public void makeValidateNews(@ModelAttribute @Valid PostNewsCreateDto createDto) {
-        Member findMember = getMember();
-        postService.makeValidateNews(createDto, findMember);
+    @GetMapping("/posts/club")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(method = "GET", summary = "동아리 작성 게시글 조회", description = "동아리 회원이 작성한 게시글들을 조회하는 API")
+    public Response findClubPost() {
+        return Response.success(postService.getClubPost());
     }
 
-    @PostMapping("/posts/valid/post")
+    @GetMapping("/admin/posts")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(method = "GET", summary = "인증 가능한 게시글 조회 - 관리자용", description = "관리자가 인증 가능한 게시글들을 조회하는 API")
+    public Response findValidPosts() {
+        return Response.success(postService.findVerifiablePost());
+    }
+
+    @PostMapping("/posts/valid/normal")
     @ResponseStatus(HttpStatus.CREATED)
-    @ApiOperation(value = "인증 가능 게시글 생성 - 일반 게시글", notes = "인증 기능한 일반 게시글을 생성하는 로직")
+    @Operation(method = "POST", summary = "인증 가능한 게시글 생성 - 일반", description = "사진을 첨부하여 인증 가능한 게시글을 작성하는 API")
     public void makeValidatePost(@ModelAttribute @Valid PostNormalCreateDto createDto) {
         Member findMember = getMember();
         postService.makeValidateNormalPost(createDto, findMember);
     }
 
-    @GetMapping("/posts/valid")
-    @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "인증 가능한 게시글 조회", notes = "인증이 가능한 게시글만을 조회하는 로직")
-    public Response findValidPosts() {
-        return Response.success(postService.findVerifiablePost());
-    }
-
-    @PutMapping("/posts/valid/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "인증 여부 확인", notes = "인증 가능한 게시글의 유효성을 판단하는 로직")
-    public void checkPostValidation(@PathVariable Long id) {
-        postService.checkPostValidation(id);
-    }
-
-    @PostMapping("/posts/normal/post")
+    @PostMapping("/posts/valid/news")
     @ResponseStatus(HttpStatus.CREATED)
-    @ApiOperation(value = "인증 불가능한 게시글 생성 - 일반 게시글", notes = "인증할 수 없는 일반 게시글을 생성하는 로직")
+    @Operation(method = "POST", summary = "인증 가능한 게시글 생성 - 뉴스", description = "뉴스 링크를 첨부하여 인증 가능한 게시글을 작성하는 API")
+    public void makeValidateNews(@ModelAttribute @Valid PostNewsCreateDto createDto) {
+        Member findMember = getMember();
+        postService.makeValidateNews(createDto, findMember);
+    }
+
+    @PostMapping("/posts/invalid/normal")
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(method = "POST", summary = "인증 불가능한 게시글 생성", description = "인증이 불가능한 게시글을 작성하는 API")
     public void makeNormalPost(@ModelAttribute @Valid PostNormalCreateDto createDto) {
         Member findMember = getMember();
         postService.makeNormalPost(createDto, findMember);
     }
 
-    @PutMapping("/posts/{id}")
+    @PutMapping("/admin/posts/valid/{id}")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "게시글 수정", notes = "게시글을 수정하는 로직")
-    public void editNewsPost(@ModelAttribute @Valid PostEditDto editDto, @PathVariable Long id) {
+    @Operation(method = "PUT", summary = "게시글 인증 - 관리자용", description = "인증 가능한 게시글을 관리자가 판단하기 위한 API")
+    public void checkPostValidation(@PathVariable Long id) {
+        postService.checkPostValidation(id);
+    }
+
+    @PutMapping("/posts/normal/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(method = "PUT", summary = "일반 게시글 수정", description = "일반 게시글을 수정하기 위한 API")
+    public void editNormalPost(@ModelAttribute @Valid PostEditDto editDto, @PathVariable Long id) {
         Member findMember = getMember();
-        postService.editPost(editDto, findMember, id);
+        postService.editNormalPost(editDto, findMember, id);
+    }
+
+    @PutMapping("/posts/news/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(method = "PUT", summary = "뉴스 게시글 수정", description = "뉴스 게시글을 수정하기 위한 API")
+    public void editNewsPost(@ModelAttribute @Valid PostNewsEditDto editDto, @PathVariable Long id) {
+        Member findMember = getMember();
+        postService.editNewsPost(editDto, findMember, id);
     }
 
     @DeleteMapping("/posts/{id}")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "게시글 삭제", notes = "게시글을 삭제하는 로직")
+    @Operation(method = "DELETE", summary = "게시글 삭제", description = "게시글을 삭제하기 위한 API")
     public void deletePost(@PathVariable Long id) {
         Member findMember = getMember();
         postService.deletePost(findMember, id);
     }
 
     private Member getMember() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = authentication.getName();
-
-        return memberRepository.findMemberByUsername(username).orElseThrow(MemberNotFoundException::new);
+        return memberFindService.findCurrentMember();
     }
 }
