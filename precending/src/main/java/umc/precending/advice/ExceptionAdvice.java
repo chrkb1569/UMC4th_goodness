c3d402eccbf740f185145fc8b34be0cc2712b2d9
minsub <chrkb1569@naver.com>
2024-02-13 17:56:46 +0900

    feat : ExceptionAdvice를 통하여 예외 처리

--- precending/src/main/java/umc/precending/advice/ExceptionAdvice.java
+++ precending/src/main/java/umc/precending/advice/ExceptionAdvice.java
@@ -5,27 +5,30 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import umc.precending.exception.recommendGoodness.CannotChangeableRecommendException;
-import umc.precending.exception.email.AuthNumNotCorrectException;
-import umc.precending.exception.member.*;
-import umc.precending.exception.person.PersonAddClubException;
-import umc.precending.exception.person.PersonAddCorporateException;
-import umc.precending.exception.category.CategoryDuplicateException;
+import umc.precending.exception.answer.AnswerNotFoundException;
+import umc.precending.exception.answer.AnswerNotMatchedWriterException;
+import umc.precending.exception.category.CategoryOutOfRangeException;
+import umc.precending.exception.cheer.CheerListEmptyException;
+import umc.precending.exception.cheer.CheerMemberNotFoundException;
+import umc.precending.exception.cheer.CheerNotFoundException;
+import umc.precending.exception.member.MemberRoleException;
 import umc.precending.exception.category.CategoryNotFoundException;
 import umc.precending.exception.image.ImageNotSupportedException;
 import umc.precending.exception.member.MemberDuplicateException;
 import umc.precending.exception.member.MemberLoginFailureException;
 import umc.precending.exception.member.MemberNotFoundException;
+import umc.precending.exception.notice.NoticeNotFoundException;
 import umc.precending.exception.post.PostAuthException;
 import umc.precending.exception.post.PostNewsNotSupportedException;
 import umc.precending.exception.post.PostNotFoundException;
 import umc.precending.exception.post.PostVerifyException;
+import umc.precending.exception.question.QuestionNotFoundException;
+import umc.precending.exception.question.QuestionNotMatchedWriterException;
 import umc.precending.exception.token.TokenNotCorrectException;
 import umc.precending.response.Response;
 
 @RestControllerAdvice
 public class ExceptionAdvice {
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Response methodArgumentNotValidException(MethodArgumentNotValidException e) {
@@ -50,6 +53,12 @@ public class ExceptionAdvice {
         return Response.failure(400, "아이디 혹은 비밀번호가 일치하지 않습니다. 다시 한 번 확인해주세요.");
     }
 
+    @ExceptionHandler(MemberRoleException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Response memberRoleException(MemberRoleException e) {
+        return Response.failure(400, e.getMessage());
+    }
+
     @ExceptionHandler(TokenNotCorrectException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Response tokenNotCorrectException() {
@@ -62,47 +71,16 @@ public class ExceptionAdvice {
         return Response.failure(400, e.getMessage());
     }
 
-    @ExceptionHandler(AuthNumNotCorrectException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Response AuthNumNotCorrect(){
-        return Response.failure(400,"인증 번호가 일치하지 않습니다");
-    }
-
-
-    @ExceptionHandler(PersonAddCorporateException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public Response MemberAddCorporateException(){
-        return Response.failure(409,"이미 이 기업을 추천하셨습니다");
-    }
-
-    @ExceptionHandler(PersonAddClubException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public Response MemberAddClubException(){
-        return Response.failure(409,"이미 이 동아리를 추천하셨습니다");
-    }
-
-    @ExceptionHandler(CannotChangeableRecommendException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public Response CannotChangeableRecommendException(){return Response.failure(409,"당신은 기회가 없습니다.");}
-
-
-    @ExceptionHandler(MemberWrongTypeException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public Response MemberWrongTypeException(){
-        return Response.failure(409,"club,corporate가 헷갈리신것 같습니다");
-    }
-
-
-    @ExceptionHandler(CategoryDuplicateException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public Response categoryDuplicateException() {
-        return Response.failure(409, "이미 존재하는 카테고리입니다.");
-    }
-
     @ExceptionHandler(CategoryNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Response categoryNotFoundException() {
         return Response.failure(404, "카테고리를 찾을 수 없습니다.");
+    }
+
+    @ExceptionHandler(CategoryOutOfRangeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Response categoryOutOfRangeException() {
+        return Response.failure(400, "입력할 수 있는 카테고리는 최소 1개, 최대 2개입니다.");
     }
 
     @ExceptionHandler(PostNotFoundException.class)
@@ -125,8 +103,55 @@ public class ExceptionAdvice {
 
     @ExceptionHandler(PostNewsNotSupportedException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Response postNewsNotSupportedException(PostNewsNotSupportedException e) {
-        return Response.failure(400, e.getMessage());
+    public Response postNewsNotSupportedException() {
+        return Response.failure(400, "회원의 정보를 다시 한 번 확인해주세요.");
     }
 
+    @ExceptionHandler(CheerMemberNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Response cheerMemberNotFoundException() {
+        return Response.failure(404, "현재 사용자가 응원하고 있는 대상이 존재하지 않습니다.");
+    }
+
+    @ExceptionHandler(CheerListEmptyException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Response cheerListEmptyException() {
+        return Response.failure(404, "현재 조건에 부합하는 대상이 존재하지 않습니다.");
+    }
+
+    @ExceptionHandler(CheerNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Response cheerNotFoundException() {
+        return Response.failure(404, "PK값과 일치하는 데이터가 존재하지 않습니다.");
+    }
+
+    @ExceptionHandler(NoticeNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Response noticeNotFoundException() {
+        return Response.failure(404, "공지 사항을 찾을 수 없습니다.");
+    }
+
+    @ExceptionHandler(QuestionNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Response questionNotFoundException() {
+        return Response.failure(404, "문의 사항을 찾을 수 없습니다.");
+    }
+
+    @ExceptionHandler(QuestionNotMatchedWriterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Response questionNotMatchedWriterException() {
+        return Response.failure(400, "문의 사항의 작성자와 현재 사용자의 정보가 일치하지 않습니다.");
+    }
+
+    @ExceptionHandler(AnswerNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Response answerNotFoundException() {
+        return Response.failure(404, "일치하는 답변을 찾을 수 없습니다.");
+    }
+
+    @ExceptionHandler(AnswerNotMatchedWriterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Response answerNotMatchedWriterException() {
+        return Response.failure(400, "답변 작성자와 현재 사용자의 정보가 일치하지 않습니다.");
+    }
 }
